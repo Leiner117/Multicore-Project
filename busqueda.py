5e023750a1a30da751cc322e638076000bc9e4e2
Stevennnncz <101902120+Stevennnncz@users.noreply.github.com>
2023-04-30 23:23:00 -0600

    A

---
+++ busqueda.py
@@ -0,0 +1,70 @@
+import threading
+import time
+
+def seleccionarModo(matriz):
+    
+    print("1. Modo Secuencial\n2. Modo Multiprocesamiento\n3. Ambos\n4. Salir")
+    select = int(input("Ingrese la opcion que desea:"))
+    busqueda = select_busqueda()
+    if select == 4:
+        return
+    
+    if (select == 1):
+        metodo_secuencial(matriz,busqueda)
+    elif (select == 2):
+        metodo_multicore(matriz, busqueda)
+    elif (select == 3):
+        metodo_secuencial(matriz,busqueda)
+        metodo_multicore(matriz,busqueda)
+    
+def select_busqueda(): 
+    print("De el dato de la persona que desea buscar")
+    busqueda = input("==> ").upper()
+    return busqueda
+
+def metodo_secuencial(matriz,busqueda):
+    print("Metodo secuencia")
+    inicio = time.time()
+
+    for i in matriz:
+        for elemento in i:
+            if busqueda.lower() in str(elemento).lower():
+                print(i)
+                break
+    
+    fin = time.time()
+    print("El codigo duro: "+str(fin-inicio))
+
+def metodo_multicore(matriz, busqueda):
+    print("Metodo Multicore")
+    inicio = time.time()
+    # número de hilos que se van a utilizar
+    num_threads = 4
+
+    def thread_function(section,busqueda):
+        for i in matriz:
+            for elemento in i:
+                if busqueda.lower() in str(elemento).lower():
+                    print(i)
+                    break
+        
+        
+
+    # dividir la matriz en secciones
+    section_size = len(matriz) // num_threads
+    sections = [matriz[i:i+section_size] for i in range(0, len(matriz), section_size)]
+
+    # crear y ejecutar los hilos
+    threads = []
+    for section in sections:
+        thread = threading.Thread(target=thread_function, args=(section, busqueda))
+        thread.start()
+        threads.append(thread)
+
+    # esperar a que los hilos terminen
+    for thread in threads:
+        thread.join()
+        
+    
+    fin = time.time()
+    print("El codigo duro: "+str(fin-inicio))
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 import cant_votantes
 import apellido_particular
 import cant_tipoCel
+import busqueda 
 # -*- coding: utf-8 -*-
 matriz = []
 with open("PADRON_COMPLETO.txt", "r") as archivo:
@@ -18,8 +19,9 @@ while op!="0":
     print("Bienvenido al sistema del padrón electoral \nIngrese la acción que desea llevar a cabo: \na. Búsqueda de una persona a partir de sus datos \nb. Cantidad de votantes por provincia \nc. Cantidad de votantes por cantón \nd. Cantidad de votantes por distritos \ne. Los N cantones con más votantes registrados\nf. Los N distritos con más votantes registrados\ng. Cantidad de personas por tipo de identificación \nh. Personas cuya identificación expira en una fecha suministrada por el usuario \ni. Cantidad de personas con un nombre en particular\nj. Cantidad de personas con un apellido en particular\nk. Los N nombres más comunes \nl. Los N apellidos más comunes \nm. Los N nombres menos comunes \nn. Una ejecución completa de las consultas")
     op = input("Ingrese su seleccion: ")
 
-    if op == "a":
-        print ("x")
+    if op == "a": 
+        busqueda.seleccionarModo(matriz)
+        input("")
     elif op == "b":
         cant_votantes.seleccionarModo(matriz,0)
     elif op == "c":
@@ -150,23 +152,6 @@ while op!="0":
             tiempo = final-inicio
             print("El tiempo de ejecucion fue de: "+str(tiempo))
         input("Presione Enter para continuar...")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     elif op == "m":
         n = input("Ingrese la cantidad de nombres que desea ver: ")
         op2 = input("Ingrese 1 si quiere que sea secuencial o 2 si quiere que sea paralelo: ")
@@ -213,9 +198,89 @@ while op!="0":
             print("El tiempo de ejecucion fue de: "+str(tiempo))
         input("Presione Enter para continuar...")
     elif op == "n":
-        print ("x")
+
+        print("Consulta B")
+        cant_votantes.metodo_multicore(matriz,0,10)
+        print ("Consulta C")
+        cant_votantes.metodo_multicore(matriz,3,10)
+        print ("Consulta D")
+        cant_votantes.metodo_multicore(matriz,4,10)
+        print ("Consulta E")
+        cant_votantes.metodo_multicore(matriz,2,10)
+        print ("Consulta F")
+        cant_votantes.metodo_multicore(matriz,1,10)
+        print ("Consulta G")
+        cant_tipoCel.metodo_multicore(matriz)
+        print ("Consulta H")
+        anio = datetime.date.year
+        mes = datetime.date.month
+        dia = datetime.date.day
+        fecha = str(anio) + str(mes) + str(dia)
+        fecha = '20230416'
+        if str(fecha) in matriz:
+            print("La fecha seleccionada no se encuentra")
+        else:
+            person = find_matching_data(matriz,fecha)
+            print(get_third_data(person))
+            print ("Consulta K")
+            n = 10
+            print("Los nombres más comunes y la cantidad de veces que aparecen son los siguientes:")
+            nombres = buscar_cuartos_campos(matriz)
+            top = find_top_x(nombres, n)
+            bool = False
+            for i in range(len(top)):
+                for j in range(len(top[0])):
+                    print(top[i][j], end=" ")
+                    if bool == True:
+                        print() 
+                    if bool == True:
+                        bool = False
+                    elif bool == False:
+                        bool = True
+            primero = top[0][1]
+            segundo = top[1][1]
+        input("Presione Enter para continuar...")
+        print ("Consulta L")
+
+        n = 10
+        print("Los apellidos más comunes y la cantidad de veces que aparecen son los siguientes:")
+
+        p_apellidos = buscar_sexto_campos(matriz)
+        s_apellidos = buscar_septimo_campos(matriz)
+
+        apellidos = p_apellidos + s_apellidos
     
+        top = find_top_x(apellidos, n)
+        bool = False
+        for i in range(len(top)):
+            for j in range(len(top[0])):
+                print(top[i][j], end=" ")
+                if bool == True:
+                    print() 
+                if bool == True:
+                    bool = False
+                elif bool == False:
+                    bool = True
+        primero = top[0][1]
+        segundo = top[1][1]
+        input("Presione Enter para continuar...")
+        print ("Consulta M")
+        n = 10
+        print("Los nombres menos comunes y la cantidad de veces que aparecen son los siguientes:")
+        nombres = buscar_cuartos_campos(matriz)
+        least = find_least_x(nombres, n)
+        bool = False
+        for i in range(len(least)):
+            for j in range(len(least[0])):
+                print(least[i][j], end=" ")
+                if bool == True:
+                    print() 
+                if bool == True:
+                    bool = False
+                elif bool == False:
+                    bool = True
 
-
-
+        primero = least[0][1]
+        segundo = least[1][1]
+        input("Presione Enter para continuar...")
 
